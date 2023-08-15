a1483e50cc40cb3a945e9442148801c08b27f3f4
AnuragAnalog <anurag.peddi1998@gmail.com>
2023-08-15 15:09:04 -0400

    added basic structure

---
+++ travel-journel/components/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function NavBar() {
+    return (
+        <div className="nav-bar">
+            <img src="../images/Globe Icon.svg" className="nav-img"/>
+            <h3 className="nav-title">my travel journel.</h3>
+        </div>
+    )
+}
---
+++ travel-journel/data.js
@@ -0,0 +1,31 @@
+data [
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    },
+    {
+        title: "Mount Everest",
+        location: "Nepal",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Everest is Earth's highest mountain above sea level, located in the Mahalangur Himal sub-range of the Himalayas. The China–Nepal border runs across its summit point. Its elevation of 8,848.86 m (29,031.7 ft) was most recently established in 2020 by the Nepali and Chinese authorities.",
+        imageUrl: "https://source.unsplash.com/8Zz5c0K1YQw"
+    },
+    {
+        title: "Mount Kilimanjaro",
+        location: "Tanzania",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Kilimanjaro or just Kilimanjaro, with its three volcanic cones, Kibo, Mawenzi, and Shira, is a dormant volcano in Tanzania. It is the highest mountain in Africa, with its summit about 4,900 metres (16,100 ft) from its base, and 5,895 metres (19,341 ft) above sea level.",
+        imageUrl: "https://source.unsplash.com/8Zz5c0K1YQw"
+    }
+]
+
+export default data;
