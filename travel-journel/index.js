43cad1e16547122cb3a5a719b271c758d1e65afd
AnuragAnalog <anurag.peddi1998@gmail.com>
2023-08-16 15:44:44 -0400

    updated a few files

--- travel-journel/index.js
+++ travel-journel/index.js
@@ -1,7 +1,7 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import App from "./App"
+// import App from "./App"
 
 ReactDOM.render(<h1>Anurag Peddi</h1>, document.getElementById("root"));
 // ReactDOM.render(<App />, document.getElementById("root"));
