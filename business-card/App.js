a7ca6918bc1e1acea43226cee3a93da1e321a808
AnuragAnalog <anurag.peddi1998@gmail.com>
2023-08-16 12:48:51 -0400

    in debugging mode

--- business-card/App.js
+++ business-card/App.js
@@ -2,44 +2,44 @@ import React from "react";
 
 function App() {
     return (
-        <div class="main-card">
-            <img class="main-image" src="./images/profile.png" />
+        <div className="main-card">
+            <img className="main-image" src="./images/profile.png" />
 
-            <div class="info">
-                <h2 class="name"> Anurag Peddi </h2>
-                <h3 class="profession"> Student </h3>
-                <a class="portfolio" href="anuraganalog.github.io"> anuraganalog.github.io </a>
+            <div className="info">
+                <h2 className="name"> Anurag Peddi </h2>
+                <h3 className="profession"> Student </h3>
+                <a className="portfolio" href="anuraganalog.github.io"> anuraganalog.github.io </a>
 
-                <div class="connect-icons">
-                    <button class="connect-icon-email" onclick="window.location.href='mailto:anurag.peddi1998@gmail.com';">
+                <div className="connect-icons">
+                    <button className="connect-icon-email" onclick="window.location.href='mailto:anurag.peddi1998@gmail.com';">
                         <img src="./images/Mail.svg" />
                         Email
                     </button>
-                    <button class="connect-icon-linkedin" onclick="window.location.href='https://www.linkedin.com/in/peddi-anurag-01767a166/';">
+                    <button className="connect-icon-linkedin" onclick="window.location.href='https://www.linkedin.com/in/peddi-anurag-01767a166/';">
                         <img src="./images/LinkedIn.svg" />
                         LinkedIn
                     </button>
                 </div>
 
-                <h3 class="sub-title-about"> About </h3>
-                <p class="info-para-about">
+                <h3 className="sub-title-about"> About </h3>
+                <p className="info-para-about">
                     I am frontend developer, with a knowledge also in
                     machine and deep learning framworks.
                 </p>
 
-                <h3 class="sub-title-interests"> Interests </h3>
-                <p class="info-para-interests">
+                <h3 className="sub-title-interests"> Interests </h3>
+                <p className="info-para-interests">
                     I am interested in learning new technologies and
                     frameworks. I have started learning cooking, and skating
                     has also been one of my favourite hobbies.
                 </p>
             </div>
 
-            <footer class="footer-icons">
-                <img class="footer-icon" src="./images/Twitter Icon.svg" />
-                <img class="footer-icon" src="./images/Instagram Icon.svg" />
-                <img class="footer-icon" src="./images/Facebook Icon.svg" />
-                <img class="footer-icon" src="./images/GitHub Icon.svg" />
+            <footer className="footer-icons">
+                <img className="footer-icon" src="./images/Twitter Icon.svg" />
+                <img className="footer-icon" src="./images/Instagram Icon.svg" />
+                <img className="footer-icon" src="./images/Facebook Icon.svg" />
+                <img className="footer-icon" src="./images/GitHub Icon.svg" />
             </footer>
         </div>
     )
