798f39b37575119b1b55311ccab0bbef728f1d22
AnuragAnalog <anurag.peddi1998@gmail.com>
2023-09-04 21:34:09 -0400

    added new project

--- travel-journel/src/components/Card.jsx
+++ travel-journel/src/components/Card.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 
-function Card() {
+function Card(props) {
     return (
         <div className="card">
-            
+            <img src={props.imageUrl} className="pic-img" />
+            <div className="loc-info">
+                <img src="/src/assets/images/Location Icon.svg" className="card-img"/>
+                <h3 className="card-location">{props.title}</h3>
+                <a href={props.googleMapsUrl} className="card-link">View on Google Maps</a>
+            </div>
+            <h1 className="card-title">{props.title}</h1>
+            <p className="card-date">{props.startDate}-{props.endDate}</p>
+            <p className="card-description">{props.description}</p>
         </div>
     )
 }
--- travel-journel/src/components/NavBar.jsx
+++ travel-journel/src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function NavBar() {
     return (
         <div className="nav-bar">
-            <img src="../images/Globe Icon.svg" className="nav-img"/>
+            <img src="/src/assets/images/Globe Icon.svg" className="nav-img"/>
             <h3 className="nav-title">my travel journel.</h3>
         </div>
     )
