cd7be679b1356f0b1e0353a2abcae03d615425cf
AnuragAnalog <anurag.peddi1998@gmail.com>
2023-09-07 00:29:38 -0400

    updated the app

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -1,14 +1,23 @@
 import React from "react";
+import memeData from '/src/memeData';
 
 function Meme() {
+    function getRandomMeme() {
+        const { memes } = memeData["data"];
+        const randomNumber = Math.floor(Math.random() * memes.length);
+        console.log(memes[randomNumber].url);
+
+        return memes[randomNumber].url;
+    };
+
     return (
         <div className="meme">
-            <form className="meme-form">
+            <div className="meme-form">
                 <input className="meme--input" type="text" placeholder="Top Text" />
                 <input className="meme--input" type="text" placeholder="Bottom Text" />
                 <button className="meme--button">Get a new meme image 🖼</button>
-            </form>
-            <img className="meme--img" src="http://i.imgflip.com/1bij.jpg" alt="Meme" />
+            </div>
+            <img className="meme--img" src={ getRandomMeme() } alt="Meme" />
         </div>
     )
 }
