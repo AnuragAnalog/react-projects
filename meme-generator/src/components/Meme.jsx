6019d045cd47b7ea0011d8c6ddb7e99eb21b9f12
AnuragAnalog <anurag.peddi1998@gmail.com>
2023-10-22 17:24:44 -0400

    changed some content

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -1,24 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import memeData from '/src/memeData';
 
 function Meme() {
-    function getRandomMeme() {
-        const { memes } = memeData["data"];
-        const randomNumber = Math.floor(Math.random() * memes.length);
-        console.log(memes[randomNumber].url);
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    });
+    const [allMemeImages, setAllMemeimages] = React.useState(memeData);
+    
+    
+    function getMemeImage() {
+        const memesArray = allMemeImages.data.memes;
+        const randomNumber = Math.floor(Math.random() * memesArray.length);
 
-        return memes[randomNumber].url;
-    };
+        setMeme(prevMeme => {
+            return {
+                ...prevMeme,
+                randomImage: memesArray[randomNumber].url
+            }
+        });
+    }
+
+    function updateText(event) {
+        const { name, value } = event.target;
+
+        setMeme(prevMeme => {
+            return {
+                ...prevMeme,
+                [name]: value
+            }
+        });
+    }
 
     return (
-        <div className="meme">
-            <div className="meme-form">
-                <input className="meme--input" type="text" placeholder="Top Text" />
-                <input className="meme--input" type="text" placeholder="Bottom Text" />
-                <button className="meme--button">Get a new meme image 🖼</button>
-            </div>
-            <img className="meme--img" src={ getRandomMeme() } alt="Meme" />
+        <main>
+        <div className="meme-form">
+            <input 
+                type="text"
+                placeholder="Top text"
+                className="meme--input"
+                name="topText"
+                onChange={updateText}
+                value={meme.topText}
+            />
+            <input 
+                type="text"
+                placeholder="Bottom text"
+                className="meme--input"
+                name="bottomText"
+                onChange={updateText}
+                value={meme.bottomText}
+            />
+            <button 
+                className="meme--button"
+                onClick={getMemeImage}
+            >
+                Get a new meme image 🖼
+            </button>
         </div>
+        <div className="meme">
+            <img src={meme.randomImage} className="meme--img" />
+            <h2 className="meme--text top">{meme.topText}</h2>
+            <h2 className="meme--text bottom">{meme.bottomText}</h2>
+        </div>
+    </main>
     )
 }
 
---
+++ quizzical/README.md
@@ -0,0 +1,2 @@
+https://opentdb.com/api_config.php
+https://www.npmjs.com/package/html-entities#user-content-decodetext-options
