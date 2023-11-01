210f307f0dfbda9073dcaf8caf99968162c231a7
AnuragAnalog <anurag.peddi1998@gmail.com>
2023-11-01 16:18:11 -0400

    added some files

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,33 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import '/src/App.css'
+
+import Dice from "/src/components/Dice.jsx"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className='main-card'>
+        <div className="game-card">
+          <div className="info">
+            {/* This is is the info */}
+          </div>
+
+          <div className="dices">
+            <Dice value={1} />
+            <Dice value={2} />
+            <Dice value={1} />
+            <Dice value={4} />
+            <Dice value={1} />
+            <Dice value={1} />
+            <Dice value={5} />
+            <Dice value={1} />
+            <Dice value={8} />
+            <Dice value={1} />
+          </div>
+
+          <div className="button">
+          </div>
+        </div>
+      </main>
     </>
   )
 }
---
+++ tenzies/src/components/Dice.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+function Dice(props) {
+    return (
+        <div className="dice">
+            <h2>
+                {props.value}
+            </h2> 
+        </div>
+    )
+}
+
+export default Dice;
